3f60e4911d4fa4fd714103b6a282f80eb6e49f88
luismagit <luismapanama@gmail.com>
2023-10-13 18:46:26 +0200

    no message

---
+++ NotesFeatures/NotesFeatures/Presentation/Utils/NotesCoreUtils.swift
@@ -0,0 +1,28 @@
+import NotesCoreUI
+import SwiftUI
+
+public enum NotesColorsUtils {
+    public static let nColors = [
+        NColors.babyBlue,
+        NColors.lightGreen,
+        NColors.redOrange,
+        NColors.redPink,
+        NColors.violet
+    ]
+
+    public static let noteColors = [
+        NoteColor.babyBlue,
+        NoteColor.lightGreen,
+        NoteColor.redOrange,
+        NoteColor.redPink,
+        NoteColor.violet
+    ]
+
+    public static let notesColorMap = [
+        NoteColor.babyBlue: NColors.babyBlue,
+        NoteColor.lightGreen: NColors.lightGreen,
+        NoteColor.redOrange: NColors.redOrange,
+        NoteColor.redPink: NColors.redPink,
+        NoteColor.violet: NColors.violet
+    ]
+}
